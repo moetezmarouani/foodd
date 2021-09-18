6bda1dcfc9f955b17a74823d2a3934c6dc033a7f
moetezmarouani <87979999+moetezmarouani@users.noreply.github.com>
2021-09-18 10:20:02 +0200

    Add files via upload

---
+++ MOMO-Food/js/main.js
@@ -0,0 +1,31 @@
+document.getElementById("inscription").addEventListener("submit" , function(event){
+    event.preventDefault();
+    var erreur;
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let email2 = document.getElementById("email2");
+    let mdp = document.getElementById("nbr");
+
+    if(document.getElementById("nbr").value==""){
+        erreur= "please put your phone number"
+    }
+    if(document.getElementById("email2").value==""){
+        erreur= "please verify your email"
+    }
+    if(document.getElementById("email").value==""){
+        erreur= "please put your email"
+    }
+    if(document.getElementById("name").value==""){
+        erreur= "please put your name"
+    }
+
+    if(erreur) {
+        event.preventDefault();
+        document.getElementById("erreur").innerHTML = erreur;
+        return false;
+    }else {
+        alert('form has been sent !');
+        erreur = "";
+    }
+    
+});
